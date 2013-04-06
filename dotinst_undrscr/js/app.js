af8574fc800b8c78f414361d876a19b681b3042a
xi17 <xizao0822@gmail.com>
2013-04-06 14:19:12 +0900

    model-added

--- dotinst_undrscr/js/app.js
+++ dotinst_undrscr/js/app.js
@@ -1,37 +1,37 @@
 (function() {
 	var Task = Backbone.Model.extend({
 		defaults:{
-			title:'do something',
+			title:"something",
 			completed:false
-		},
+		}
 	});
-	var task1 = new Task();
+	var Tasks = Backbone.Collection.extend({model:Task});
 
-
-	console.log(task1.toJSON());
-
-
-	//View
 	var TaskView = Backbone.View.extend({
 		tagName:'li',
-		className:'liClass',
-		id:'liId',
-		events:{
-			"click .command":"sayHello"
-		},
-		sayHello:function(){
-			alert('hello');
-		},
-		template:_.template($('#task-template').html()),
-		render: function(){
+		template:_.template($('#task-template').html());
+		render:function(){
 			var template = this.template(this.model.toJSON());
 			this.$el.html(template);
 			return this;
 		}
 	});
 
-	var taskView = new TaskView({ model:task1});
-	console.log(taskView.render().el);
-	$('body').append(taskView.render().el);
+    var TasksView = Backbone.View.extend({});
+
+	var tasks = new Tasks([
+		{
+			title:"task1",
+			completed:true
+		},
+		{
+			title:"task2"
+		},
+		{
+			title:"task3"
+		}
+	]);
+
+
 
 })();
